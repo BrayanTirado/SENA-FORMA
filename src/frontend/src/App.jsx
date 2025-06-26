c7f0741c7df57fb908eda01e9fec8f48bdf57299
Joshuaswift19 <147520210+Joshuaswift19@users.noreply.github.com>
2025-06-26 17:12:53 -0500

    New page Forgotten Password

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SenaFormacionComplementaria from './pages/SenaFormacionComplementaria';
 import Dashboard from './pages/Dashboard';
 import Register from './pages/Register';
+import ForgottenPassword from './pages/ForgottenPassword';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route path="/sena-formacion-complementaria" element={<SenaFormacionComplementaria />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgottenPassword />} />
+
       </Routes>
     </Router>
   );
